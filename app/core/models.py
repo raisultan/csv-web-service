7ddfb0141453f32ac0a7fb8f159f38adc6ab8865
Raysultan Karimov <ki.xbozz@gmail.com>
2020-01-25 18:06:02 +0700

    models added

--- app/core/models.py
+++ app/core/models.py
@@ -1,12 +1,36 @@
 from django.db import models
 
 
+class Item(models.Model):
+  name = models.CharField(max_length=255)
+
+  def __str__(self):
+    return self.name
+
+
+class Client(models.Model):
+  username = models.CharField(max_length=255)
+  spent_money = models.IntegerField(default=0)
+  gems = models.ManyToManyField(Item, related_name='clients')
+
+  def __str__(self):
+    return self.username
+
+
 class Deal(models.Model):
-  customer_name = models.CharField(max_length=255)
-  item = models.CharField(max_length=255)
+  client = models.ForeignKey(
+    Client,
+    on_delete=models.CASCADE,
+    related_name='deals'
+  )
+  item = models.ForeignKey(
+    Item,
+    on_delete=models.CASCADE,
+    related_name='deals'
+  )
   total = models.IntegerField()
   quantity = models.IntegerField()
   date = models.DateTimeField()
 
   def __str__(self):
-    return self.customer_name
+    return f'{self.date} {self.client} {self.item}'
